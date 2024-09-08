1198e43a75c09f0a24e055b743af9c981691384a
Aakash Kumar <aakashkumar113450@gmail.com>
2024-09-08 07:17:12 +0530

    pushisaving files....

---
+++ admin/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Componenets/Navbar/Navbar";
+import Admin from "./Pages/Admin/Admin";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <Admin/>
+      </div>
+    </>
+  );
+}
+
+export default App;
